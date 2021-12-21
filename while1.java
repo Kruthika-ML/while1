ba0176f747666158edb6b3a92daab29ec7f38652
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-21 19:31:55 +0530

    Add files via upload

---
+++ while1.java
@@ -0,0 +1,9 @@
+public class while1 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 7){
+            System.out.println("Values are " +count);
+            count++;
+        }
+    }
+}
---
+++ while2.java
@@ -0,0 +1,12 @@
+public class while2 {
+    public static void main(String[] args) {
+        //isEvenNumber(4);
+        int num = 1;
+        while (num % 2 == 0) {
+            // num = 1;
+            System.out.println(num);
+            num++;
+
+        }
+    }
+}
